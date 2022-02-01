e826b512602b427f36b4504e398859cc9aa1e1ff
michaeldegraciacuento <amfufu.michael@gmail.com>
2022-02-01 21:36:27 +0800

    [update] hide welcome view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,12 +14,12 @@
     <link href="{{ asset('css/frontend.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<body >
+<body>
     <div class="row">
-        <div class="col-md-7 login-img">
-           <div class="card mt-3 " style="width: 60%;margin-left:20%;opacity:60%;color:white;background-color: rgba(0,0,0,.6);">
+        <div class="col-sm-7 login-img">
+           <div class="card mt-3" style="width: 40%;margin-left:30%;opacity:60%;color:white;background-color: rgba(0,0,0,.6);">
                 <div class="card-body">
-                    <div id="MyClockDisplay" class="clock" onload="showTime()"></div>
+                    <div id="MyClockDisplay" class="text-center clock" onload="showTime()"></div>
                 </div>
            </div>
             {{-- <img src="{{ asset('img/ngcp2.jpg') }}" alt="ngcp1" class="login-img float-start"> --}}
